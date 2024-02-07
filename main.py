58e6ca30847700601014f40d338a819758b238da
Moez Ali <moez.ali@queensu.ca>
2024-02-07 09:54:29 -0500

    all set

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-# print("hello")
-
-# calculator.py
+# my function
 
 
-def add(a, b):
+def my_function(a, b):
     return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero.")
-    return a / b
---
+++ test/test_main.py
@@ -0,0 +1,21 @@
+# Inside test_main.py
+
+import sys
+import os
+
+# Add the parent directory to sys.path to allow importing from the parent directory
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+# Now you can import my_function from main.py
+from main import my_function
+
+
+def test_my_function():
+    # Test with integers
+    assert my_function(2, 3) == 5
+    # Test with floats
+    assert my_function(2.5, 3.5) == 6.0
+    # Test with negative numbers
+    assert my_function(-2, -3) == -5
